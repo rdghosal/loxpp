e930c820ad78948895deab57aab05aded196e52e
Rahul Ghosal <rdghosal@proton.me>
2026-05-24 23:54:59 -0400

    feat(scanner): consume numerics and idents, completing scanner

--- src/lox/scanner.cpp
+++ src/lox/scanner.cpp
@@ -1,8 +1,19 @@
 #include "scanner.hpp"
 #include "lox.hpp"
+#include <print>
+#include <unordered_map>
 #include <variant>
 
 Scanner::Scanner(std::string_view src) : source_{src} {}
+
+const std::unordered_map<std::string_view, TokenType> Scanner::keywords_{
+    {"and", TokenType::AND},     {"class", TokenType::CLASS},   {"else", TokenType::ELSE},
+    {"false", TokenType::FALSE}, {"fun", TokenType::FUN},       {"for", TokenType::FOR},
+    {"if", TokenType::IF},       {"nil", TokenType::NIL},       {"or", TokenType::OR},
+    {"print", TokenType::PRINT}, {"return", TokenType::RETURN}, {"super", TokenType::SUPER},
+    {"this", TokenType::THIS},   {"true", TokenType::TRUE},     {"var", TokenType::VAR},
+    {"while", TokenType::WHILE}, {"eof_", TokenType::EOF_},
+};
 
 std::vector<Token> Scanner::scan_tokens() {
     while (!is_at_end()) {
@@ -60,14 +71,95 @@ void Scanner::scan_token() {
     case '>':
         add_token(match('=') ? TokenType::GREATER_EQUAL : TokenType::GREATER);
         break;
+    case '/':
+        if (match('/')) {
+            // Handle comments
+            while (peek() != '\n' && is_at_end()) {
+                std::println("peeking...");
+                advance();
+            }
+        } else {
+            add_token(TokenType::SLASH);
+        }
+        break;
+    case ' ':
+    case '\r':
+    case '\t':
+        break;
+    case '\n':
+        line_++;
+        break;
+    case '"':
+        string();
+        break;
     default:
-        Lox::error(line_, "Unexpected character.");
+        if (is_digit(c)) {
+            number();
+        } else if (is_alpha(c)) {
+            identifier();
+        } else {
+            Lox::error(line_, "Unexpected character.");
+        }
         break;
     }
 }
 
 char Scanner::advance() {
     return source_[current_++];
+}
+
+void Scanner::string() {
+    while (peek() != '"' && !is_at_end()) {
+        if (peek() == '\n') {
+            line_++;
+        }
+        advance();
+    }
+    if (is_at_end()) {
+        Lox::error(line_, "Unterminated string.");
+        return;
+    }
+    // Consume the closing quote
+    advance();
+
+    // Trim surroudning quotes
+    std::string value{source_.substr(start_ + 1, current_ - 1)};
+    add_token(TokenType::STRING, value);
+}
+
+void Scanner::number() {
+    while (is_digit(peek())) {
+        advance();
+    }
+
+    if (peek() == '.' && is_digit(peek_next())) {
+        // Consume decimal
+        advance();
+    }
+    while (is_digit(peek())) {
+        advance();
+    }
+
+    // Trim surroudning quotes
+    double value{
+        std::stod(std::string{
+            source_.substr(start_, current_ - start_),
+        }),
+    };
+    add_token(TokenType::NUMBER, value);
+}
+
+void Scanner::identifier() {
+    while (is_alphanumeric(peek())) {
+        advance();
+    }
+    auto text = source_.substr(start_, current_ - start_);
+    auto token_t = keywords_.find(text);
+    if (token_t != keywords_.end()) {
+        add_token(token_t->second);
+    } else {
+        add_token(TokenType::IDENTIFIER);
+    }
 }
 
 void Scanner::add_token(TokenType type) {
@@ -94,6 +186,27 @@ void Scanner::add_token(TokenType type, Literal literal) {
     });
 }
 
+char Scanner::peek() {
+    return (is_at_end()) ? '\0' : source_.at(current_);
+}
+
+char Scanner::peek_next() {
+    return (current_ + 1 >= source_.size()) ? '\0' : source_.at(current_ + 1);
+}
+
+bool Scanner::is_digit(char c) {
+    return c >= '0' && c <= '9';
+}
+
+bool Scanner::is_alpha(char c) {
+    return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c == '_'));
+}
+
+bool Scanner::is_alphanumeric(char c) {
+    return is_digit(c) || is_alpha(c);
+}
+
 bool Scanner::is_at_end() {
+
     return current_ >= source_.size();
 }
--- src/lox/scanner.hpp
+++ src/lox/scanner.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "token.hpp"
 #include <string_view>
+#include <unordered_map>
 #include <vector>
 
 class Scanner {
@@ -14,10 +15,19 @@ class Scanner {
     std::size_t start_{0};
     std::size_t current_{0};
     std::size_t line_{1};
+    static const std::unordered_map<std::string_view, TokenType> keywords_;
     bool is_at_end();
     void scan_token();
     char advance();
     void add_token(TokenType type);
     void add_token(TokenType type, Literal literal);
+    char peek();
+    char peek_next();
     bool match(char expected);
+    void string();
+    void number();
+    void identifier();
+    static bool is_digit(char c);
+    static bool is_alpha(char c);
+    static bool is_alphanumeric(char c);
 };
